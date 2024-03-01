4f000008b0dd85ac84f369dd7e868c682c29388f
caique-dev-01 <caique.alves02@gmail.com>
2024-03-01 16:26:53 -0300

    atualizado

---
+++ repeticao.js
@@ -0,0 +1,7 @@
+for(let i =0; i <=10; i++)
+document.getElementById("tabuada").innerHTML = "<p>"
+ +"<p> 2x0=" +(2*0) +"<P>"
+ +"<p> 2x1=" +(2*1) +"<P>"
+ +"<p> 2x2=" +(2*2) +"<P>"
+ +"<p> 2x3=" +(2*3) +"<P>"
+ +"<p> 2x4=" +(2*4) +"<P>"
